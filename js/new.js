55820fd75b5cd5f936e841de26e372381a7da84e
sabiku85 <kurzynska.sabina@gmail.com>
2018-09-27 11:06:07 +0200

    flexbox, basic JS - start button, cardboard, cards rotation

--- js/new.js
+++ js/new.js
@@ -1,7 +1,16 @@
+const picsArray = ['images/bananas.png', 'images/birdy.png', 'images/coconut.png', 'images/eggplant.png', 'images/elephant.png', 'images/mushroom.png', 'images/olives.png', 'images/pineapples.png', 'images/bananas.png', 'images/birdy.png', 'images/coconut.png', 'images/eggplant.png', 'images/elephant.png', 'images/mushroom.png', 'images/olives.png', 'images/pineapples.png'];
+
+
 function showCards() {
-  console.log('ok');
+  // console.log('ok');
   $('.welcome').addClass('hidden');
   $('.card').removeAttr('hidden');
 }
 
+function rotateCardAndShowPic() {
+  // console.log('ok');
+  $(this).toggleClass('rotate');
+}
+
 $('#start').on('click', showCards);
+$('.card').on('click', rotateCardAndShowPic);
